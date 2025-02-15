5d33743475ac536b8ed79200783816bfd86159a4
Shee3py <96235652+Shee3py@users.noreply.github.com>
2025-02-15 22:29:34 +0400

    added buttons and changed the score counting to accomodate UI

---
+++ rps.js
@@ -0,0 +1,75 @@
+let humanScore = 0;
+let computerScore = 0;
+let result;
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+function getComputerChoice(){
+    let randnum =  Math.floor(Math.random() * 3);
+    if (randnum === 0) {
+        return "rock";
+    } else if (randnum === 1) {
+        return "paper";
+    }
+    else {
+        return "scissors";
+    }
+}
+
+/*function getHumanChoice(){
+    let choice;
+    while (!((choice == "paper") || (choice =="rock") || (choice =="scissors"))){
+        choice = prompt("Choose Rock, Paper or Scissors").toLowerCase();
+    }
+    
+    return choice;
+}*/
+
+function playRound(humanChoice, computerChoice){
+    computerChoice = getComputerChoice();
+    if (humanChoice == computerChoice){
+        checkScore()
+        return;
+    } else if ((humanChoice == "rock" && computerChoice == "scissors") || (humanChoice == "paper" && computerChoice == "rock") || ((humanChoice == "scissors" && computerChoice == "paper"))) {
+        humanScore = humanScore + 1;
+        checkScore()
+        return;
+    } else {
+        computerScore = computerScore + 1;
+        checkScore()
+        return;
+    }
+}
+
+/*function playGame()
+{
+    for (let i = 0; i < 5; i++){
+        playRound(getHumanChoice(), getComputerChoice())
+    }
+    
+}*/
+
+function checkScore(){
+    if ((humanScore === 5) || (computerScore === 5)){
+        resultScore();
+    }
+}
+
+function resultScore(){
+    if (humanScore > computerScore){
+        result = "You Won !!!";
+    }else if (computerScore > humanScore){
+        result = "You Lost";
+    }else {
+        result = "Draw";
+    }
+    console.log(result);
+}
+
+//rock.onclick = playRound("rock");
+
+rock.addEventListener("click", () => playRound("rock"));
+paper.addEventListener("click", () => playRound("paper"));
+scissors.addEventListener("click", () => playRound("scissors"));
